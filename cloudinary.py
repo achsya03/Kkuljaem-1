2a3d4a64e1afbdfdab70511ec8156cd146265002
achsya03 <ach.sya03@gmail.com>
2021-07-13 20:53:53 +0700

    first commit

---
+++ cloudinary.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template, request
+if request.method == 'POST':
+    file_to_upload = request.files['file']
+
+if file_to_upload:
+    upload_result = cloudinary.uploader.upload(file_to_upload)
+    app.logger.info(upload_result)
+
+
+@app.route("/upload", methods=['POST'])
+def upload_file():
+  app.logger.info('in upload route')
+
+  cloudinary.config(cloud_name = os.getenv('CLOUD_NAME'), api_key=os.getenv('API_KEY'), 
+    api_secret=os.getenv('API_SECRET'))
+  upload_result = None
+  if request.method == 'POST':
+    file_to_upload = request.files['file']
+    app.logger.info('%s file_to_upload', file_to_upload)
+    if file_to_upload:
+      upload_result = cloudinary.uploader.upload(file_to_upload)
+      app.logger.info(upload_result)
+      return jsonify(upload_result)
